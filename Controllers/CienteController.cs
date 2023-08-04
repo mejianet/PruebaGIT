8a23348941848dd913bc9813e20008ed94c4f2d0
Miguel Mauricio Mejía M <mmejia@evolution-it.com.co>
2023-08-04 12:17:20 -0500

    Add project files.

---
+++ Controllers/CienteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CienteController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public dynamic GetClientes()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente()
+                {
+                    Id = 1,
+                    Name = "Miguel",
+                    Description = "xcosa"
+                },
+                new Cliente()
+                {
+                    Id = 2,
+                    Name = "Javier",
+                    Description = "xcosa"
+                }
+            };
+            return clientes;
+        }
+    }
+}
